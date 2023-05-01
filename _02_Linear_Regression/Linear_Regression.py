695c1c2ec5d371d00b7bdab70db31866a8a036ae
wh1stle1 <106904643+wh1stle1@users.noreply.github.com>
2023-05-01 16:50:27 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -14,8 +14,14 @@ def ridge(data):
     return weight @ data
     
 def lasso(data):
+    learning_rate = 1e-10
+    max_iter = 10000
+    alpha = 0.1
     X, y = read_data()
-    weight = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, y))
+    weight = np.zeros(X.shape[1])
+    for i in range(max_iter):
+        gradient = np.dot(X.T, (np.dot(X, weight) - y)) + alpha * np.sign(weight)
+        weight =weight - learning_rate * gradient
     return weight @ data
 
 def read_data(path='./data/exp02/'):
